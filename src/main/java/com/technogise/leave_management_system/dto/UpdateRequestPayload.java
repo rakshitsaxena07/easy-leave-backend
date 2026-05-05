86bd1e844c3d075353c0800cf5ec25252c18955f
RajDeshmukh2001 <rajdd61@gmail.com>
2026-05-05 20:19:37 +0530

    refactor(LMS-28): add validation to UpdateRequestPayload fields

--- src/main/java/com/technogise/leave_management_system/dto/UpdateRequestPayload.java
+++ src/main/java/com/technogise/leave_management_system/dto/UpdateRequestPayload.java
@@ -2,6 +2,8 @@ package com.technogise.leave_management_system.dto;
 
 import com.technogise.leave_management_system.enums.RequestStatus;
 import com.technogise.leave_management_system.enums.RequestType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,9 +14,12 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 public class UpdateRequestPayload {
+    @NotNull(message = "Request type must not be null")
     private RequestType requestType;
 
+    @NotNull(message = "Request status must not be null")
     private RequestStatus status;
 
+    @Size(max = 1000, message = "Remark must not exceed 1000 characters")
     private String managerRemark;
 }
